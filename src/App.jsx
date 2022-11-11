4a41864d69fe951e891b9ab07f079baa83d2d615
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-11 11:38:27 +0100

    npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-import { useContext, useState } from "react";
-import { Level1 } from "./components/Level1";
-import { THEME, ThemeModeContext } from "./contexts/ThemeModeContext";
-
 export function App() {
-  const initialThemeMode = useContext(ThemeModeContext);
-  const [themeMode, setThemeMode] = useState(initialThemeMode);
-  return (
-    <ThemeModeContext.Provider value={{ themeMode, setThemeMode }}>
-      <div
-        style={{
-          color: THEME[themeMode].textColor,
-          backgroundColor: THEME[themeMode].backgroundColor,
-          height: "100vh",
-        }}
-      >
-        <Level1 />
-      </div>
-    </ThemeModeContext.Provider>
-  );
+  return <h1>Hello</h1>;
 }
