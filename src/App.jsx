d194721b46ccf62de0faeadb421be2251fa20cfe
codiku-dev <codiku.dev@gmail.com>
2022-12-02 18:01:51 +0100

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
+import { useRef } from "react";
 export function App() {
-  return <h1>Hello</h1>;
+
+  const codeInputRef = useRef();
+  const dateInputRef = useRef();
+
+  function handleCardNumberChange(e) {
+    if (e.target.value.length >= 16) {
+      codeInputRef.current.focus();
+    }
+  }
+
+
+  function handleCodeChange(e) {
+    if (e.target.value.length >= 3) {
+      dateInputRef.current.focus();
+    }
+  }
+
+  function handleDateChange(e) {
+    if (e.target.value.length >= 4) {
+      dateInputRef.current.blur();
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <label>Code carte bancaire</label>
+        <input
+          autoFocus
+          onChange={handleCardNumberChange}
+          type="number"
+          name="creditCardNumber"
+        />
+      </div>
+      <div>
+        <label>Code à 3 chiffres</label>
+        <input onChange={handleCodeChange} ref={codeInputRef} type="number" name="creditCardCode" />
+      </div>
+      <div>
+        <label>Expiration</label>
+        <input onChange={handleDateChange} ref={dateInputRef} type="text" name="creditCardCode" />
+      </div>
+
+    </>
+  );
+
 }
